34f08e072f987194209544b2c8dbbeac2d434bbf
jonamartino <jonamartino@gmail.com>
2021-07-06 03:37:00 -0300

    Funcionalidad de seleccion de fichas y movimiento en las celdas permitidas.. falta regla para comer fichas

--- app.js
+++ app.js
@@ -1,56 +1,184 @@
-window.onload = function() {
+const RED_CLASS = 'has-red-piece'
+const WHITE_CLASS = 'has-white-piece'
+let redTurn
+var aux = '';
 
-    /*var board = document.getElementById("board")
+
+/*function handleClick(e){
+
     
-    var row = document.createElement('div')
-    row.classname = "board__row row1"
+    const cell = e.target
+    if(e.target.classList.contains('has-red-piece')){
+        selected(cell)
+        console.log(e.target.index)
+        currentClass = 'has-red-piece';
+       // console.log(cell.parentNode.previousSibling.childNodes)
+       
+       // e.target.classList.remove('has-red-piece')
 
-    var cell1 = document.createElement('div')
-    cell.classname = "row__item item1-1"
-
-    var cell2 = document.createElement('div')
-    cell2.classname = "row__item item-2 has-white-piece"
+    }
+    else if(e.target.classList.contains('has-white-piece')){
+        selected(cell)
+        console.log("blanca")
+        currentClass = 'has-white-piece';
+    }
     
-    row.appendChild(cell1)
-    row.appendChild(cell2)
+    placeMark(cell, currentClass)
+    //swapTurns()
+    
+    console.log('clicked')
+    //placemark
+    //check for win
+    //check for draw
+    //switch turns
+}*/
 
-    board.appendChild(row)*/
+function selected(cell){
+    cell.classList.add("selected")
+    pieceSelected = true
+}
 
-    //1 = ficha roja
-    //2 = ficha blanca
-    //0 = vacio
-    var stateGame = [
-        [0,2,0,2,0,2,0,2],
-        [2,0,2,0,2,0,2,0],
-        [0,2,0,2,0,2,0,2],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [1,0,1,0,1,0,1,0],
-        [0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0],
-    ]
+/*
+function swapTurns(){
+    redTurn = !redTurn
+}*/
+//null mov invalido
+var stateGame = [
+    [null,2,null,2,null,2,null,2],
+    [2,null,2,null,2,null,2,null],
+    [null,2,null,2,null,2,null,2],
+    [0,null,0,null,0,null,0,null],
+    [null,0,null,0,null,0,null,0],
+    [1,null,1,null,1,null,1,null],
+    [null,1,null,1,null,1,null,1],
+    [1,null,1,null,1,null,1,null],
+]
 
+var pieceSelected = false
+
+var yselected = null
+var xselected = null
+
+
+function printBoard(){
     var board = document.getElementById("board")
-    
     stateGame.forEach(function(row, j){
         var rowElement = document.createElement('div')
-        rowElement.className = "board__row row" + (j + 1)
-        
-
+        rowElement.classList.add("board__row")
+        rowElement.classList.add("row" + (j))
         row.forEach(function(cell, i){
             var cellElement = document.createElement('div')
-            cellElement.className = "row__item item" + (j + 1) + "-" + (i + 1)
-
+            cellElement.classList.add("row__item")
+            cellElement.classList.add("item" + (j) + "-" + (i))
             if(cell == 1){
-                cellElement.className += " has-red-piece"
+                cellElement.classList.add("has-red-piece")
             }else if(cell == 2){
-                cellElement.className += " has-white-piece"
+                cellElement.classList.add("has-white-piece")
             }
 
-            rowElement.appendChild(cellElement)
-          
+
+            rowElement.appendChild(cellElement) 
         })
         board.appendChild(rowElement)
     })
-   
 }
+
+function test(){
+    document.getElementById("board").addEventListener('click',posicion)
+
+}
+
+
+
+
+function posicion(e){
+
+    ar = stateGame
+    var board = document.getElementById("board")
+    var x = event.x;
+    var y = event.y;
+    //resto 5 porque no se como sacar el margen
+    x = x - board.offsetLeft - 5;
+    y = y - board.offsetTop - 5;
+
+    var xtransf = (Math.floor(x/60))
+    var ytransf = (Math.floor(y/60))
+    //redondeo la division con el Math.floor
+    if(!pieceSelected && (ar[ytransf][xtransf] ==1 )){
+        selected(e.target)
+        console.log("ficha:" +ar[ytransf][xtransf] +" x:"+ xtransf + "y:"+ ytransf + "|||" +xselected+":"+yselected);
+        currentClass = "has-red-piece";
+        yselected = ytransf;
+        xselected = xtransf;
+        
+               
+    }else if(!pieceSelected && (ar[ytransf][xtransf] ==2 )){
+        selected(e.target)
+        console.log("ficha:" +ar[ytransf][xtransf] +" x:"+ xtransf + "y:"+ ytransf + "|||" +xselected+":"+yselected);
+        currentClass = "has-white-piece";
+        yselected = ytransf;
+        xselected = xtransf;
+        
+    }
+
+    
+
+    if(pieceSelected && (ar[ytransf][xtransf] ==0 )){
+        if(currentClass== "has-red-piece"){
+            ar[ytransf][xtransf]=1
+            ar[yselected][xselected]=0
+            pieceSelected = false
+            console.log(ar[yselected][xselected], "has red piece")
+            pintaTablero(ar)
+        }else if(currentClass== "has-white-piece"){
+            ar[ytransf][xtransf]=2
+            ar[yselected][xselected]=0
+            pieceSelected = false
+            console.log(ar[yselected][xselected], "has white piece")
+            pintaTablero(ar)
+        }
+    }
+
+    
+    console.log()
+    
+}
+
+
+
+
+function pintaTablero(ar){
+    var board = document.getElementById("board")
+    while (board.firstChild) {
+        board.removeChild(board.lastChild);
+      }
+    //limpia tablero para crearlo con la modificacion 
+    ar.forEach(function(row, j){
+        var rowElement = document.createElement('div')
+        rowElement.classList.add("board__row")
+        rowElement.classList.add("row" + (j))
+        row.forEach(function(cell, i){
+            var cellElement = document.createElement('div')
+            cellElement.classList.add("row__item")
+            cellElement.classList.add("item" + (j) + "-" + (i))
+            if(cell == 1){
+                cellElement.classList.add("has-red-piece")
+            }else if(cell == 2){
+                cellElement.classList.add("has-white-piece")
+            }
+
+
+            rowElement.appendChild(cellElement) 
+        })
+        board.appendChild(rowElement)
+    })
+
+    console.log(ar)
+}
+
+window.onload = function() {
+    printBoard()
+    test()
+    
+}
+
